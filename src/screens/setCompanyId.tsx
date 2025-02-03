7afe4aeaa2402ec55ded7443f8879bc898ab1ef9
Osman <othmanghandour51@gmail.com>
2025-02-04 01:45:48 +0200

    feat: create ui & navigation functionality to set company id screen

--- src/screens/setCompanyId.tsx
+++ src/screens/setCompanyId.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import {Text, View} from 'react-native';
 import {styles} from '../styles';
+import NavigateButton from '../components/NavigateButton';
+import {ROUTES} from '../constants/routes';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {SettingsStackParamList} from '../types';
 
-const SetCompanyId = () => {
+type SetCompanyIdScreenProps = NativeStackScreenProps<
+  SettingsStackParamList,
+  'setcompanyid'
+>;
+
+const SetCompanyId = ({navigation}: SetCompanyIdScreenProps) => {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Set Company Id Screen</Text>
+      <NavigateButton
+        label="Continue"
+        onPress={() => navigation.push(ROUTES.ENTER_COMPANY_ID)}
+      />
     </View>
   );
 };

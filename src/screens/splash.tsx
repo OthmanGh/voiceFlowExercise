68bc87a87f3bcebcff341d9b0b4854bcab6dcb5f
Osman <othmanghandour51@gmail.com>
2025-02-03 22:07:08 +0200

    feat: add navigation type and redirect functionality

--- src/screens/splash.tsx
+++ src/screens/splash.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Image, Text, View} from 'react-native';
 import {styles} from '../styles';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {Mic} from '../constants/images';
+import {ROUTES} from '../constants/routes';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {OnBoardingStackParamList} from '../types';
 
-const Splash = () => {
+type SplashScreenProps = NativeStackScreenProps<
+  OnBoardingStackParamList,
+  'splash'
+>;
+
+const Splash = ({navigation}: SplashScreenProps) => {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigation.replace(ROUTES.WELCOME);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, [navigation]);
+
   return (
     <SafeAreaView style={[styles.container, styles.splashContainer]}>
       <Image source={Mic} style={styles.image} />

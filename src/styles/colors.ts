7f8baf970e1283c64926ed91c9b5177bf857a823
Osman <othmanghandour51@gmail.com>
2025-02-03 20:40:51 +0200

    style: define reusable color palette in COLORS object

---
+++ src/styles/colors.ts
@@ -0,0 +1,11 @@
+export const COLORS = {
+  primary: '#633CFF',
+  primary_light: '#BEADFF',
+  primary_extra_light: '#EFEBFF',
+
+  dark: '#222',
+  dark_gray: '#737373',
+  gray: '#D9D9D9',
+  light_gray: '#FAFAFA',
+  white: '#FFFFFF',
+} as const;

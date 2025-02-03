80d26f7bfe24c2259a23984f90ee53db701a0b73
Osman <othmanghandour51@gmail.com>
2025-02-03 22:18:56 +0200

    feat: create custom navigate button

---
+++ src/components/NavigateButton.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {StyleSheet, Text, TouchableOpacity} from 'react-native';
+import {COLORS} from '../styles/colors';
+
+interface NavigateButtonProps {
+  label: string;
+  onPress: () => void;
+}
+
+const NavigateButton = ({label, onPress}: NavigateButtonProps) => {
+  return (
+    <TouchableOpacity onPress={onPress} style={styles.navigateBtn}>
+      <Text style={styles.navigateBtnText}>{label}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default NavigateButton;
+
+const styles = StyleSheet.create({
+  navigateBtn: {
+    marginVertical: 20,
+    backgroundColor: COLORS.primary,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 6,
+  },
+
+  navigateBtnText: {
+    fontSize: 18,
+    color: COLORS.light_gray,
+    fontWeight: '500',
+  },
+});

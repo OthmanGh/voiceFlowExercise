a6eaff10e2b59d24ab4e0f2cb602cc853f147d6e
Osman <othmanghandour51@gmail.com>
2025-02-04 01:42:36 +0200

    style: modify screen header options for stack screens

--- src/navigation/settingsStack.tsx
+++ src/navigation/settingsStack.tsx
@@ -6,22 +6,53 @@ import SetCompanyId from '../screens/setCompanyId';
 import PickVoice from '../screens/pickVoice';
 import EnterCompanyId from '../screens/enterCompanyId';
 import {SettingsStackParamList} from '../types';
+import {COLORS} from '../styles/colors';
 
 const SettingsStack = createNativeStackNavigator<SettingsStackParamList>();
 
 const settingsStack = () => {
   return (
     <SettingsStack.Navigator>
-      <SettingsStack.Screen name={ROUTES.SETTINGS} component={Settings} />
+      <SettingsStack.Screen
+        name={ROUTES.SETTINGS}
+        component={Settings}
+        options={{
+          headerShown: false,
+          animation: 'slide_from_bottom',
+          presentation: 'modal',
+          gestureDirection: 'vertical',
+        }}
+      />
+
       <SettingsStack.Screen
         name={ROUTES.SET_COMPANY_ID}
         component={SetCompanyId}
+        options={{
+          title: '',
+          headerShadowVisible: false,
+          headerTintColor: COLORS.dark_gray,
+        }}
       />
+
       <SettingsStack.Screen
         name={ROUTES.ENTER_COMPANY_ID}
         component={EnterCompanyId}
+        options={{
+          title: '',
+          headerShadowVisible: false,
+          headerTintColor: COLORS.dark_gray,
+        }}
       />
-      <SettingsStack.Screen name={ROUTES.PICK_VOICE} component={PickVoice} />
+
+      <SettingsStack.Screen
+        name={ROUTES.PICK_VOICE}
+        component={PickVoice}
+        options={{
+          title: '',
+          headerShadowVisible: false,
+          headerTintColor: COLORS.dark_gray,
+        }}
+      />
     </SettingsStack.Navigator>
   );
 };

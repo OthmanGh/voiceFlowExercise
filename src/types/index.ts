32814f201d444890d5ed7f493361cc95b2f3877e
Osman <othmanghandour51@gmail.com>
2025-02-03 22:06:14 +0200

    feat: add onboarding stack param list type definition

---
+++ src/types/index.ts
@@ -0,0 +1,8 @@
+import {ROUTES} from '../constants/routes';
+
+export type OnBoardingStackParamList = {
+  [ROUTES.SPLASH]: undefined;
+  [ROUTES.WELCOME]: undefined;
+  [ROUTES.ENTER_COMPANY_ID]: undefined;
+  [ROUTES.PICK_VOICE]: undefined;
+};

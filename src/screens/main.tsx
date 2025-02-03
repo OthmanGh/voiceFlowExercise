92085ef6e6ed9b6e15ef2c55f7750be92c758678
Osman <othmanghandour51@gmail.com>
2025-02-04 00:04:02 +0200

    feat: create main screen ui & functionality

--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, TouchableOpacity, View, Image} from 'react-native';
 import {styles} from '../styles';
+import {settingsImg} from '../constants/images';
+import NavigateButton from '../components/NavigateButton';
+import {ROUTES} from '../constants/routes';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {RootStackParamList} from '../types';
 
-const Main = () => {
+type MainScreenProps = NativeStackScreenProps<RootStackParamList, 'main'>;
+
+const Main = ({navigation}: MainScreenProps) => {
   return (
     <View style={styles.container}>
+      <TouchableOpacity style={styles.imageBtn}>
+        <Image source={settingsImg} style={styles.settingsImg} />
+      </TouchableOpacity>
       <Text style={styles.text}>Main Screen</Text>
+      <NavigateButton
+        label="Launch Voicebot screen"
+        onPress={() => navigation.push(ROUTES.VOICE_BOT)}
+      />
     </View>
   );
 };
+
 export default Main;

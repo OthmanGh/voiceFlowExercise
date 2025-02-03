e4f18eac7430f6780e7d233625c5b71cce834862
Osman <othmanghandour51@gmail.com>
2025-02-03 20:56:50 +0200

    feat: implement OnBoardingStack navigator

---
+++ src/navigation/onBoardingStack.tsx
@@ -0,0 +1,27 @@
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import React from 'react';
+import Splash from '../screens/splash';
+import {ROUTES} from '../constants/routes';
+import Welcome from '../screens/welcome';
+import EnterCompanyId from '../screens/enterCompanyId';
+import PickVoice from '../screens/pickVoice';
+
+const OnBoardingStack = createNativeStackNavigator();
+
+const onBoardingStack = () => {
+  return (
+    <OnBoardingStack.Navigator initialRouteName={ROUTES.SPLASH}>
+      <OnBoardingStack.Screen name={ROUTES.SPLASH} component={Splash} />
+
+      <OnBoardingStack.Screen name={ROUTES.WELCOME} component={Welcome} />
+
+      <OnBoardingStack.Screen
+        name={ROUTES.ENTER_COMPANY_ID}
+        component={EnterCompanyId}
+      />
+
+      <OnBoardingStack.Screen name={ROUTES.PICK_VOICE} component={PickVoice} />
+    </OnBoardingStack.Navigator>
+  );
+};
+export default onBoardingStack;

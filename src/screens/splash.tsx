f84e9bdc8f0181c42884abbfd894d0af6c1dd0dc
Osman <othmanghandour51@gmail.com>
2025-02-03 21:48:03 +0200

    style: create splash screen ui & add it's styles

--- src/screens/splash.tsx
+++ src/screens/splash.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Image, Text, View} from 'react-native';
 import {styles} from '../styles';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {Mic} from '../constants/images';
 
 const Splash = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Splash Screen</Text>
-    </View>
+    <SafeAreaView style={[styles.container, styles.splashContainer]}>
+      <Image source={Mic} style={styles.image} />
+      <View>
+        <Text style={styles.logoText}>
+          <Text style={styles.voiceText}>Voice</Text>
+          <Text style={styles.flowText}>Flow</Text>
+        </Text>
+
+        <Text style={styles.descriptionText}>
+          Elevating Your Voice Experience with Advanced AI (dummy text)
+        </Text>
+      </View>
+
+      <Text style={styles.screenText}>(Splash Screen)</Text>
+    </SafeAreaView>
   );
 };
+
 export default Splash;
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,10 +1,54 @@
 import {StyleSheet} from 'react-native';
+import {COLORS} from './colors';
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  screenText: {
+    position: 'absolute',
+    fontWeight: '500',
+    color: COLORS.light_gray,
+    bottom: 30,
+    fontSize: 20,
+  },
+
+  splashContainer: {
+    backgroundColor: COLORS.primary,
+  },
+
+  image: {
+    width: 100,
+    height: 100,
+    marginBottom: 40,
+  },
+
+  logoText: {
+    fontSize: 36,
+    fontWeight: '700',
+    color: COLORS.white,
+    marginBottom: 10,
+    textAlign: 'center',
+  },
+
+  voiceText: {
+    color: COLORS.dark,
+  },
+
+  descriptionText: {
+    fontSize: 17,
+    fontWeight: '500',
+    color: COLORS.primary_extra_light,
+    textAlign: 'center',
+    lineHeight: 24,
+    maxWidth: '80%',
+  },
+
+  flowText: {
+    color: COLORS.white,
   },
 
   text: {

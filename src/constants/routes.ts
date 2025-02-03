48f62115c8ba662d0dd09367993da30ffa126aad
Osman <othmanghandour51@gmail.com>
2025-02-03 20:53:15 +0200

    feat: define app navigation routes in constants

---
+++ src/constants/routes.ts
@@ -0,0 +1,11 @@
+export const ROUTES = {
+  SPLASH: 'splash',
+  WELCOME: 'welcome',
+  ENTER_COMPANY_ID: 'entercompanyid',
+  PICK_VOICE: 'pickvoice',
+  MAIN: 'main',
+  VOICE_BOT: 'voicebot',
+  SETTINGS: 'settings',
+  ON_BOARDING_STACK: 'onboardingstack',
+  SETTINGS_STACK: 'settingsstack',
+} as const;

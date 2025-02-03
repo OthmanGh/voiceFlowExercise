80452978b3b1cb5dfed0db025c1dcf18fabe0918
Osman <othmanghandour51@gmail.com>
2025-02-04 00:55:59 +0200

    fix: add missing setCompanyId route

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -2,6 +2,7 @@ export const ROUTES = {
   SPLASH: 'splash',
   WELCOME: 'welcome',
   ENTER_COMPANY_ID: 'entercompanyid',
+  SET_COMPANY_ID: 'setcompanyid',
   PICK_VOICE: 'pickvoice',
   MAIN: 'main',
   VOICE_BOT: 'voicebot',

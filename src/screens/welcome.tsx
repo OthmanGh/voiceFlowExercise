4f387a63771d4c4e20b5da2ac5c4793b33b7339d
Osman <othmanghandour51@gmail.com>
2025-02-04 00:04:45 +0200

    feat: implement welcome screen ui & functionality

--- src/screens/welcome.tsx
+++ src/screens/welcome.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import {Text, View} from 'react-native';
 import {styles} from '../styles';
+import NavigateButton from '../components/NavigateButton';
+import {ROUTES} from '../constants/routes';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {OnBoardingStackParamList} from '../types';
 
-const Welcome = () => {
+type WelcomeScreenProps = NativeStackScreenProps<
+  OnBoardingStackParamList,
+  'welcome'
+>;
+
+const Welcome = ({navigation}: WelcomeScreenProps) => {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Welcome Screen</Text>
+      <NavigateButton
+        label="Get Started"
+        onPress={() => {
+          navigation.push(ROUTES.ENTER_COMPANY_ID);
+        }}
+      />
     </View>
   );
 };

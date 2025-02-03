7808fe0ff71f966378fb4f52a43d90462a20f2eb
Osman <othmanghandour51@gmail.com>
2025-02-03 22:08:04 +0200

    feat: add prop type definition

--- src/navigation/onBoardingStack.tsx
+++ src/navigation/onBoardingStack.tsx
@@ -5,13 +5,18 @@ import {ROUTES} from '../constants/routes';
 import Welcome from '../screens/welcome';
 import EnterCompanyId from '../screens/enterCompanyId';
 import PickVoice from '../screens/pickVoice';
+import {OnBoardingStackParamList} from '../types';
 
-const OnBoardingStack = createNativeStackNavigator();
+const OnBoardingStack = createNativeStackNavigator<OnBoardingStackParamList>();
 
 const onBoardingStack = () => {
   return (
     <OnBoardingStack.Navigator initialRouteName={ROUTES.SPLASH}>
-      <OnBoardingStack.Screen name={ROUTES.SPLASH} component={Splash} />
+      <OnBoardingStack.Screen
+        name={ROUTES.SPLASH}
+        component={Splash}
+        options={{headerShown: false}}
+      />
 
       <OnBoardingStack.Screen name={ROUTES.WELCOME} component={Welcome} />
 
